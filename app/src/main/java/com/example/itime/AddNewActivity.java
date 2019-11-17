183f90beaeb41cc9fe1b1fb98ba4b5a76941158c
Uzumaki1010 <13616919952@163.com>
2019-11-17 21:20:42 +0800

    完成了AddNewActivity中两个按钮的响应事件

--- app/src/main/java/com/example/itime/AddNewActivity.java
+++ app/src/main/java/com/example/itime/AddNewActivity.java
@@ -2,7 +2,9 @@ package com.example.itime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 
@@ -19,8 +21,26 @@ public class AddNewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_new);
 
         floatingActionButtonBack=(FloatingActionButton)findViewById(R.id.floating_action_button_back);
-        floatingActionButtonBack=(FloatingActionButton)findViewById(R.id.floating_action_button_ok);
+        floatingActionButtonOK=(FloatingActionButton)findViewById(R.id.floating_action_button_ok);
         editTextTitle=(EditText)findViewById(R.id.edit_text_title);
         editTextNote=(EditText)findViewById(R.id.edit_text_note);
+
+        floatingActionButtonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddNewActivity.this.finish();
+            }
+        });
+
+        floatingActionButtonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.putExtra("title",editTextTitle.getText().toString());
+                intent.putExtra("note",editTextNote.getText().toString());
+                setResult(RESULT_OK,intent);
+                AddNewActivity.this.finish();
+            }
+        });
     }
 }
